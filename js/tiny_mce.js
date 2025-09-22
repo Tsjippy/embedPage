b3162027e7ad11af538aecd7b0a6002a679d9773
Tsjippy <ewaldha@hotmail.com>
2025-09-22 11:20:32 +0100

    fixed: niceselect

--- js/tiny_mce.js
+++ js/tiny_mce.js
@@ -49,11 +49,11 @@ tinymce.create(
                     dialog = editor.windowManager.open(pageEmbedDialog);
                     let select						= document.querySelector('.wp-editor-help [name="page-selector"]');
 					Main.attachNiceSelect(select);
-					let niceselect 					= select._niceselect.dropdown
-					niceselect.style.position		= 'relative';
-					niceselect.style.width			= "200px";
-					niceselect.style.border			= '2px solid #303030';
-					niceselect.style.marginBottom	= '10px';
+					let niceSelect 					= select._niceSelect.dropdown
+					niceSelect.style.position		= 'relative';
+					niceSelect.style.width			= "200px";
+					niceSelect.style.border			= '2px solid #303030';
+					niceSelect.style.marginBottom	= '10px';
                 }
             );
             
