9049cd46027345621b117b1eaa92812268236ced
Tsjippy <ewaldha@hotmail.com>
2026-04-26 19:56:41 +0200

    changed: block apt to version 3

--- blocks/embedPage/build/index.asset.php
+++ blocks/embedPage/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8e0b032d00d2acfcc382');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '8e0618f251972b24cdc6');
